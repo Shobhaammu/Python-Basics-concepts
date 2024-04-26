d270e99e93f48d83107e8dfa04630585484ae18e
Shobhaammu <167527377+Shobhaammu@users.noreply.github.com>
2024-04-26 10:05:08 +0530

    Delete untitled3.py

--- untitled3.py
+++
@@ -1,9 +0,0 @@
-w=int(input('weight of watermellon:'))
-if(w%2!=0):
-    print('no')
-else:
-    x=w/2
-    if (x%2==0):
-        print('x1= ',x,'and x2= ',x)
-    else:
-        print('x1= ',x-1,'and x2= ',x+1)
