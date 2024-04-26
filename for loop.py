7b001a95e0cb8a267306be3ac637e19973e6e403
Shobhaammu <167527377+Shobhaammu@users.noreply.github.com>
2024-04-26 09:49:56 +0530

    Delete for loop.py

--- for loop.py
+++
@@ -1,7 +0,0 @@
-fruits=['apple','banana','cherry','kiwi']
-for x in fruits:
-    if x=='banana':
-        print('the fruit is present')
-    else:
-        print('the fruit is not present')
-print(fruits)        
